6038edbcf0c50b2d16bbe70797ece71e4ebd27d0
gbcardoso <gbcardoso96@gmail.com>
2017-10-24 17:00:21 +0100

    TCP interface

--- src/RMI_Interface_TCP.java
+++ src/RMI_Interface_TCP.java
@@ -1,5 +1,13 @@
 import java.rmi.*;
+import java.util.HashMap;
 
 public interface RMI_Interface_TCP extends Remote {
-	public String message() throws java.rmi.RemoteException;
+	public boolean login(String username, String password )throws RemoteException;
+	public int getUserType(String username) throws RemoteException;
+	public HashMap<Integer, String> getElections(int usertype) throws RemoteException;
+	public HashMap<Integer, String> getListsElections(int usertype, int idElection) throws RemoteException;
+	public boolean vote(int userId, int idElection, int idList) throws RemoteException; //false se ja existir voto
+	//public void unlockTable() throws; <-- so do lado do tcp :)
+	public boolean logout()throws RemoteException;
+
 }
